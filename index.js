c1cef7bef5d88ce86dff7425393fd7231b442cfd
agrawlins <agrawlins@gmail.com>
2021-08-02 21:10:56 -0400

    created an app.post and app.put feature for index.js. Also used Postman to create/update a new item in products.json

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const repoContext = require('./repository/repository-wrapper');
 const app = express();
 
+app.use(express.json());
+
+app.use(express.urlencoded({extended:true}));
+
 app.listen(3000, function () {
     console.log("server started. Listening on port 3000.");
 });
@@ -10,4 +14,17 @@ app.get('/api/products/:id', (req, res) => {
     const id = req.params.id;
     const products = repoContext.products.findProductById(id);
     return res.send(products);
+});
+
+app.post('/api/products', (req, res) => {
+    const newProduct = req.body;
+    const addedProduct = repoContext.products.createProduct(newProduct);
+    return res.send(addedProduct);
+})
+
+app.put('/api/products/:id', (req, res) => {
+    const id = req.params.id;
+    const productPropertiesToUpdate = req.body;
+    const updatedProduct = repoContext.products.updateProduct(id, productPropertiesToUpdate);
+    return res.send(updatedProduct);
 });
