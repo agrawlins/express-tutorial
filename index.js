b36de5df54dcc1e9fb5f1aae061b7f9ef0a96515
agrawlins <agrawlins@gmail.com>
2021-08-02 21:50:58 -0400

    installed cors and added it as a const in index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { resolveMx } = require('dns');
 const express = require('express');
 const repoContext = require('./repository/repository-wrapper');
+const cors = require('cors');
+const {validateProduct} = require('./middleware/products-validation');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use(express.urlencoded({extended:true}));
 
 app.listen(3000, function () {
@@ -17,13 +19,13 @@ app.get('/api/products/:id', (req, res) => {
     return res.send(products);
 });
 
-app.post('/api/products', (req, res) => {
+app.post('/api/products', [validateProduct], (req, res) => {
     const newProduct = req.body;
     const addedProduct = repoContext.products.createProduct(newProduct);
     return res.send(addedProduct);
 })
 
-app.put('/api/products/:id', (req, res) => {
+app.put('/api/products/:id', [validateProduct], (req, res) => {
     const id = req.params.id;
     const productPropertiesToUpdate = req.body;
     const updatedProduct = repoContext.products.updateProduct(id, productPropertiesToUpdate);
